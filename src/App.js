5df4d66759d522a65a7d664af2b7ef04b9da5b3a
morwen80 <ribesnero80@gmail.com>
2019-09-26 13:54:48 +0100

    added CSS for login and signup buttons - border-radius and padding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import React from 'react';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
@@ -9,21 +11,19 @@ import AddPrompt from './components/AddPrompt';
 import Header from './components/Header';
 import PageNotFound from './components/PageNotFound';
 
-
 const App = () => {
   return (
     <AuthProvider>
-    <Router>
-    <Header />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/signup' component={Signup} />
-
-        <PrivateRoute exact path='/new' component={AddPrompt} />
-        <Route component={PageNotFound} />
-        </Switch>
-    </Router>
+      <Router>
+        <Header />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={Signup} />
+            <PrivateRoute exact path='/new' component={AddPrompt} />
+            <Route component={PageNotFound} />
+          </Switch>
+      </Router>
     </AuthProvider>
   );
 }
